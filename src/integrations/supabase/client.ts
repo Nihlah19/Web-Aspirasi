06931b71441cb3ad9d43715e404cd53c063d8aa1
Nihlah.A <auliyanihlah.com>
2026-06-18 00:10:06 +0800

    hm

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,10 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://hvmjakwrmfcrsgbpyexl.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2bWpha3dybWZjcnNnYnB5ZXhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1Nzk4NjEsImV4cCI6MjA5NzE1NTg2MX0.khVvI1UKkSv81wVBfb2i8FWtn42x0nRVOSEj49123dg";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
